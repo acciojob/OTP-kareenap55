dcce33135aa81abc408cc51d606fc95b3789b9fd
kareenap55 <kareenap1412@gmail.com>
2025-03-28 13:03:20 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    const inputs = document.querySelectorAll(".code");
+    
+    inputs.forEach((input, index) => {
+        input.addEventListener("input", (e) => {
+            if (e.inputType === "insertText" && e.data && index < inputs.length - 1) {
+                inputs[index + 1].focus();
+            }
+        });
+
+        input.addEventListener("keydown", (e) => {
+            if (e.key === "Backspace") {
+                if (input.value === "" && index > 0) {
+                    inputs[index - 1].focus();
+                }
+            }
+        });
+    });
+});
+

e3e4661752c6c70acd16354c12fb45ee6a88875b
kareenap55 <kareenap1412@gmail.com>
2025-04-05 02:03:04 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,33 @@
-//your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function () {
-    const inputs = document.querySelectorAll(".code");
+const inputs = document.querySelectorAll(".code");
 
-    inputs.forEach((input, index) => {
-        input.addEventListener("input", (e) => {
-            const value = e.target.value;
-            if (/^\d$/.test(value)) {
-                input.value = value;
-                if (index < inputs.length - 1) {
-                    inputs[index + 1].focus();
-                }
-            } else {
-                input.value = "";
-            }
-        });
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    const value = e.target.value;
+    if (value.match(/[0-9]/)) {
+      if (index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    } else {
+      e.target.value = ""; // clear invalid input
+    }
+  });
 
-        input.addEventListener("keydown", (e) => {
-            if (e.key === "Backspace") {
-                if (input.value === "") {
-                    if (index > 0) {
-                        inputs[index - 1].focus();
-                        inputs[index - 1].value = "";
-                    }
-                } else {
-                    input.value = "";
-                }
-                e.preventDefault();
-            }
-        });
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "") {
+        if (index > 0) {
+          inputs[index - 1].focus();
+          inputs[index - 1].value = "";
+        }
+      } else {
+        input.value = "";
+      }
+    } else if (e.key >= "0" && e.key <= "9") {
+      input.value = ""; // overwrite any existing value
+    }
+  });
+});
 
-        input.addEventListener("paste", (e) => {
-            e.preventDefault();
-            const pasteData = e.clipboardData.getData("text").replace(/\D/g, "");
-            if (pasteData.length === inputs.length) {
-                inputs.forEach((inp, i) => {
-                    inp.value = pasteData[i] || "";
-                });
-                inputs[inputs.length - 1].focus();
-            }
-        });
-    });
+window.addEventListener("load", () => {
+  inputs[0].focus();
 });
